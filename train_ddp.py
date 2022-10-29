5f014d16b1dac45d5b1a8d751119c88658da9b44
Yukarin <mistelinn@gmail.com>
2022-10-29 08:51:38 +0300

    Add model loading

--- train_ddp.py
+++ train_ddp.py
@@ -5,29 +5,10 @@ import os
 import torch
 import torch.nn as nn
 import torch.distributed as dist
-import torchvision
-import torchvision.transforms as transforms
 import torch.multiprocessing as mp
 
 from model import HAT
 from data import SQLDataset
-
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--nodes', default=1, type=int, metavar='N')
-    parser.add_argument('-g', '--gpus', default=1, type=int,
-                        help='number of gpus per node')
-    parser.add_argument('-nr', '--nr', default=0, type=int,
-                        help='ranking within the nodes')
-    parser.add_argument('--epochs', default=2, type=int, metavar='N',
-                        help='number of total epochs to run')
-    args = parser.parse_args()
-
-    args.world_size = args.gpus * args.nodes
-    os.environ['MASTER_ADDR'] = '127.0.0.1'
-    os.environ['MASTER_PORT'] = '29500'
-    mp.spawn(train, nprocs=args.gpus, args=(args,))
 
 
 def train(gpu, args):
@@ -50,26 +31,27 @@ def train(gpu, args):
                 mlp_ratio=2, upsampler='pixelshuffle', resi_connection='1conv')
     torch.cuda.set_device(gpu)
     model.cuda(gpu)
-    batch_size = 2
+
+    # Wrapper around our model to handle parallel training
+    model = nn.parallel.DistributedDataParallel(model, device_ids=[gpu])
+
+    # load
+    dist.barrier()
+    map_location = {'cuda:%d' % 0: 'cuda:%d' % rank}
+    model.load_state_dict(
+        torch.load('model-l-ddp.pth', map_location=map_location))
 
     # define loss function (criterion) and optimizer
     criterion = nn.L1Loss().cuda(gpu)
     optimizer = torch.optim.Adam(model.parameters(), 2e-4)
 
-    # Wrapper around our model to handle parallel training
-    model = nn.parallel.DistributedDataParallel(model, device_ids=[gpu])
-
     # Data loading code
+    batch_size = 2
     train_dataset = SQLDataset('/kaggle/input/ani2k-p64-x2/train_p64_x2.db', hflip=False, vflip=False, rotate=False)
-    train_sampler = torch.utils.data.distributed.DistributedSampler(
-        train_dataset,
-        num_replicas=args.world_size,
-        rank=rank
-    )
+    train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                                batch_size=batch_size,
                                                shuffle=False,
-                                               num_workers=0,
                                                pin_memory=True,
                                                sampler=train_sampler)
     if gpu == 0:
@@ -78,6 +60,7 @@ def train(gpu, args):
     start = datetime.now()
     total_step = len(train_loader)
     for epoch in range(args.epochs):
+        train_sampler.set_epoch(epoch)
         for i, (images, labels) in enumerate(train_loader):
             images = images.cuda(non_blocking=True)
             labels = labels.cuda(non_blocking=True)
@@ -101,6 +84,23 @@ def train(gpu, args):
                 torch.save(model.state_dict(), 'model.pth')
     if gpu == 0:
         print("Training complete in: " + str(datetime.now() - start))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--nodes', default=1, type=int, metavar='N')
+    parser.add_argument('-g', '--gpus', default=1, type=int,
+                        help='number of gpus per node')
+    parser.add_argument('-nr', '--nr', default=0, type=int,
+                        help='ranking within the nodes')
+    parser.add_argument('--epochs', default=2, type=int, metavar='N',
+                        help='number of total epochs to run')
+    args = parser.parse_args()
+
+    args.world_size = args.gpus * args.nodes
+    os.environ['MASTER_ADDR'] = '127.0.0.1'
+    os.environ['MASTER_PORT'] = '29500'
+    mp.spawn(train, nprocs=args.gpus, args=(args,))
 
 
 if __name__ == '__main__':
