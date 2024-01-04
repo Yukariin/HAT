ebd832d9e548af6f1fcf66315f311173dd71e161
Yukari Yakumo <mistelinn@gmail.com>
2024-01-04 16:03:54 +0300

    Add GradientVariance

--- loss.py
+++ loss.py
@@ -157,8 +157,8 @@ def msssim(img1, img2, window_size=11, size_average=True, y_channel=False):
 
     pow1 = mcs ** weights
     pow2 = mssim ** weights
-    output = torch.prod(pow1[:-1] * pow2[-1])
-    return output
+    input = torch.prod(pow1[:-1] * pow2[-1])
+    return input
 
 
 def psnr(input, target, y_channel=False, max_val=1.):
@@ -221,3 +221,41 @@ def psnrb(input, target, y_channel=False, max_val=1.):
         total += 10. * torch.log10(max_val / (mse + bef))
 
     return total / input.shape[1]
+
+
+# https://github.com/lusinlu/gradient-variance-loss/blob/main/gradient_variance_loss.py
+class GradientVariance(nn.Module):
+    def __init__(self, patch_size, cpu=False):
+        super().__init__()
+
+        self.patch_size = patch_size
+
+        self.kernel_x = torch.FloatTensor([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]).unsqueeze(0).unsqueeze(0)
+        self.kernel_y = torch.FloatTensor([[1, 2, 1], [0, 0, 0], [-1, -2, -1]]).unsqueeze(0).unsqueeze(0)
+        if not cpu:
+            self.kernel_x = self.kernel_x.cuda()
+            self.kernel_y = self.kernel_y.cuda()
+
+        self.unfold = torch.nn.Unfold(kernel_size=(self.patch_size, self.patch_size), stride=self.patch_size)
+    
+    def forward(self, input, target):
+        gray_input = 0.2989 * input[:, 0:1, :, :] + 0.5870 * input[:, 1:2, :, :] + 0.1140 * input[:, 2:, :, :]
+        gray_target = 0.2989 * target[:, 0:1, :, :] + 0.5870 * target[:, 1:2, :, :] + 0.1140 * target[:, 2:, :, :]
+
+        gx_target = F.conv2d(gray_target, self.kernel_x, stride=1, padding=1)
+        gy_target = F.conv2d(gray_target, self.kernel_y, stride=1, padding=1)
+        gx_output = F.conv2d(gray_input, self.kernel_x, stride=1, padding=1)
+        gy_output = F.conv2d(gray_input, self.kernel_y, stride=1, padding=1)
+
+        gx_target_patches = self.unfold(gx_target)
+        gy_target_patches = self.unfold(gy_target)
+        gx_output_patches = self.unfold(gx_output)
+        gy_output_patches = self.unfold(gy_output)
+
+        var_target_x = torch.var(gx_target_patches, dim=1)
+        var_output_x = torch.var(gx_output_patches, dim=1)
+        var_target_y = torch.var(gy_target_patches, dim=1)
+        var_output_y = torch.var(gy_output_patches, dim=1)
+
+        gradvar_loss = F.mse_loss(var_target_x, var_output_x) + F.mse_loss(var_target_y, var_output_y)
+        return gradvar_loss
